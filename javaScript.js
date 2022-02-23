c41929ab9dd75017c20a7033d461734f05c14a33
musayuksel <musayuxel@gmail.com>
2022-02-23 16:40:02 +0000

    Add getNames fucntion

--- javaScript.js
+++ javaScript.js
@@ -5,6 +5,17 @@ function sum() {
 }
 
 //Question
+function getNames(arr) {
+  // input array of object
+  // getNames([
+  //   {a:1},
+  //   {name:"Mike",lastName:"Yxl"},
+  //   {name:"John"},
+  //   {age:35}
+  // ]);
+  //return array of names if exist ["Mike","John"]
+}
+//Question
 function findDublicates(arr) {
   // input numberof array =>[1,2,3,2,4,4,5,6,7,8,5]
   //return dublicates as an array [2,4,5]
