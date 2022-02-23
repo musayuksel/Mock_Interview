a0f259dfba59d5da41e1a57df0c2d867c8d2609d
musayuksel <musayuxel@gmail.com>
2022-02-23 16:32:58 +0000

    Add sum function

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,10 @@
 //Question
+function sum() {
+  //input any number of arg sum(2,3) Or sum(2,3,4)
+  //return sum of them 5 or 9
+}
+
+//Question
 function findDublicates(arr) {
   // input numberof array =>[1,2,3,2,4,4,5,6,7,8,5]
   //return dublicates as an array [2,4,5]
