ab0cf677cb55166728a4b99d7a487787adb892f0
Musa <79163385+musayuksel@users.noreply.github.com>
2024-07-11 11:57:59 +0100

    Add test setup for TDD

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 //Question
-function sum() {
+function sumNumbers() {
   //input any number of arg sum(2,3) Or sum(2,3,4)
   //return sum of them 5 or 9
 }
@@ -16,7 +16,9 @@ function getNames(arr) {
   //return array of names if exist ["Mike","John"]
 }
 //Question
-function findDublicates(arr) {
-  // input numberof array =>[1,2,3,2,4,4,5,6,7,8,5]
-  //return dublicates as an array [2,4,5]
+function findDuplicates(arr) {
+  // input number of array =>[1,2,3,2,4,4,5,6,7,8,5]
+  //return duplicates as an array [2,4,5]
 }
+
+module.exports = { sumNumbers, getNames, findDuplicates };
---
+++ javascript/script.test.js
@@ -0,0 +1,37 @@
+const { sumNumbers, getNames } = require("./script");
+
+describe("sumNumbers function:", () => {
+  test("should return sum when two arguments are provided", () => {
+    // Assert sum of two numbers
+  });
+
+  test("should return argument itself when one arguments are provided", () => {
+    // Assert return argument itself if there is one argument
+  });
+
+  test("should return 0 when no arguments are provided", () => {
+    // Assert return 0 when no argument
+  });
+
+  test("should return sum when more arguments are provided", () => {
+    // Assert sum of three numbers
+  });
+});
+
+describe("getNames function:", () => {
+  test("should return empty array when the argument is empty array", () => {
+    // Assert
+  });
+
+  test("should return array of names when the argument array objects has name", () => {
+    // Assert
+  });
+
+  test("should return empty array when the argument array objects has no name", () => {
+    // Assert
+  });
+
+  test("should return only array of names when the some argument array objects has not name ", () => {
+    // Assert
+  });
+});
